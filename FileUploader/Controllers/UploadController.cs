1c359ae105b3c10dd6b6e17bf112ad32ae582560
Alex Skiba <aliaksei.skiba@albelli.com>
2019-03-30 11:58:59 +0100

    Initial implementation

---
+++ FileUploader/Controllers/UploadController.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            using (var fileStream = new FileStream("bin/tmp.csv", FileMode.Create))
+            {
+                await Request.Body.CopyToAsync(fileStream);
+                fileStream.Flush();
+            }
+
+            return Ok();
+        }
+    }
+}
